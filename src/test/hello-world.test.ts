4f0b5fcb7980a6892f0974e68fa72175f10d17da
dan-ueno <ueno2005@gmail.com>
2021-11-05 15:18:59 -0300

    hello world expect with chai API

--- src/test/hello-world.test.ts
+++ src/test/hello-world.test.ts
@@ -11,11 +11,10 @@ describe('hello-world test', function () {
             en
           }
         }`;
+    const expectedResponse = { data: { hello: { ptBr: 'olar', en: 'Hello, World' } } };
+    const response = await request('localhost:4001').post('/').send({ query });
 
-    await request('localhost:4001')
-      .post('/')
-      .send({ query })
-      .expect(200)
-      .expect({ data: { hello: { ptBr: 'olar', en: 'Hello, World' } } });
+    expect(response.statusCode).to.equal(200);
+    expect(JSON.stringify(response.body)).to.equal(JSON.stringify(expectedResponse));
   });
 });
