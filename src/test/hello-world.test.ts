beed6eab3855519582243d51035b3f4959aa7808
dan-ueno <ueno2005@gmail.com>
2021-11-05 13:23:47 -0300

    test at hello-world query done

--- src/test/hello-world.test.ts
+++ src/test/hello-world.test.ts
@@ -1,13 +1,13 @@
 import { setup } from '../setup';
 const request = require('supertest');
 
-// before(() => {
-//   setup();
-// });
+before(() => {
+  setup();
+});
 
 describe('Hello-World', function () {
   describe('hello-world test', function () {
-    it('hello-world response', function (done) {
+    it('hello-world response', async function () {
       //meu teste aqui
       const query = `
       query
@@ -18,14 +18,11 @@ describe('Hello-World', function () {
           }
         }`;
 
-      request('localhost:4001')
+      await request('localhost:4001')
         .post('/')
-        .send(query)
+        .send({ query })
         .expect(200)
-        .end(function (err) {
-          if (err) return done(err);
-          return done();
-        });
+        .expect({ data: { hello: { ptBr: 'olar', en: 'Hello, World' } } });
     });
   });
 });
