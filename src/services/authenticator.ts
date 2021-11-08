6b4ff73a5e3e852cf3fee8f0f5b399b7a93a616c
dan-ueno <ueno2005@gmail.com>
2021-11-08 10:06:35 -0300

    created authentication service

---
+++ src/services/authenticator.ts
@@ -0,0 +1,19 @@
+import * as jwt from 'jsonwebtoken';
+
+export interface AuthenticationData {
+  id: string;
+}
+
+export class Authenticator {
+  public generate(input: AuthenticationData): string {
+    const token = jwt.sign(input, process.env.JWT_KEY, {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRES_IN,
+    });
+    return token;
+  }
+
+  public getTokenData(token: string): AuthenticationData {
+    const data = jwt.verify(token, process.env.JWT_KEY);
+    return data as AuthenticationData;
+  }
+}
